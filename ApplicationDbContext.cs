e13daba6e9a953fa9e3fe026a195b0c878de8613
Ahmed Metwally <153832364+githupAhmedMetwally@users.noreply.github.com>
2024-05-07 23:55:19 +0300

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1Ef
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public DbSet<TaaskEntity> MyProperty { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=.\DotNet;Initial Catalog=Task-1;
+                  Integrated Security=True;TrustServerCertificate=True");
+        }
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TaaskEntity>()
+                .ToTable("NewTask");
+
+           modelBuilder.Entity<TaaskEntity>()
+                       .Property(p => p.Date)  
+                    .HasColumnName("Deadline");
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_1Ef
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             
+        }
+    }
+}
